ec7917c89410502b4275113e7b486242b8ca63ac
janickkiy <janickiy@mail.ru>
2026-06-27 00:57:23 +0300

    bugfix

--- resources/views/icons/background/account.blade.php
+++ resources/views/icons/background/account.blade.php
@@ -1,7 +1,6 @@
 <svg xmlns="http://www.w3.org/2000/svg" class="{{ isset($class) ? $class : '' }}" viewBox="0 0 100 100" aria-hidden="true">
-    <rect width="100" height="100" rx="35" style="opacity:0.10"/>
-    <circle cx="50" cy="38" r="13"/>
-    <path d="M25 74c3-16 16-25 25-25s22 9 25 25" fill="none" stroke="currentColor" stroke-width="8" stroke-linecap="round"/>
-    <path d="M62 60h5a6 6 0 0 1 0 12h-5M55 72h-5a6 6 0 0 1 0-12h5" fill="none" stroke="currentColor" stroke-width="5" stroke-linecap="round"/>
-    <path d="M55 66h7" fill="none" stroke="currentColor" stroke-width="5" stroke-linecap="round"/>
+    <rect width="100" height="100" rx="35" fill="#10b981" style="opacity:0.14"/>
+    <rect x="30" y="27" width="40" height="46" rx="8" fill="#10b981"/>
+    <circle cx="50" cy="43" r="8" fill="#fff"/>
+    <path d="M37 64c2-10 24-10 26 0v2H37Z" fill="#fff"/>
 </svg>
